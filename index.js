7778c929eeb2be2952ec7a4ff5d5976bc38fb247
samg613 <samuelg380@nycstudents.net>
2024-10-24 13:58:27 -0400

    bd

---
+++ index.js
@@ -0,0 +1,9 @@
+function binarytoDecimal(math) {
+  let decimal = 0;
+  for (i = 0; i < length; i++) {
+    decimal += Number(c[length - 1 - i]) * Math.pow(2, i);
+
+    return decimal;
+  }
+  console.log(binarytoDecimal(11));
+}
