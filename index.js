c074681cf5c7cb0cf009a13c55ec3787b5d8be70
samg613 <samuelg380@nycstudents.net>
2024-10-25 13:54:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function binarytoDecimal(math) {
   }
   console.log(binarytoDecimal(11));
 }
+
+
+//whalen func
+function binarytoDecimal(binary) {
+  let decimal = 0;
+  for (i = 0; i < length; i++) {
+  if (binary[binary.length - i- 1]) * Math.pow(2, i);
+
+    return decimal;
+  }
+}
+let x = "1011";
+x[x.length - i - 1];
